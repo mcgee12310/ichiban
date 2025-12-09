572336ff6d44d9f950ad7407dcca3f55278d059d
giapts <nguyentronggiap317@gmail.com>
2025-12-09 22:57:12 +0700

    Mo ta thay doi cua ban

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import './App.css'
 import Auth from "./user/components/Auth";
 import EventsSearch from './user/pages/searchPage/EventsSearch';
+import PlaceDetailPage from "./user/pages/Detail";
 
 function App() {
   return (
     <Routes>
       <Route path="/auth" element={<Auth/>}/>
       <Route path="/events/search" element={<EventsSearch/>}/>
+      <Route path="/detail" element={<PlaceDetailPage/>}/>
     </Routes>
   )
 }
