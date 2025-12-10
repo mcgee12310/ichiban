6612a945ef1ce066120efc5bab04da1424df9500
Amyl11 <vumanhcvp@gmail.com>
2025-12-10 10:27:32 +0700

    Homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,12 @@ import { Routes, Route } from "react-router-dom";
 import './App.css'
 import Auth from "./user/components/Auth";
 import EventsSearch from './user/pages/searchPage/EventsSearch';
+import HomePage from './user/pages/homePage/HomePage';
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<HomePage/>}/>
       <Route path="/auth" element={<Auth/>}/>
       <Route path="/events/search" element={<EventsSearch/>}/>
     </Routes>
